5377d3dd38ab6598b794fb7cae5cd49cb22ef9ce
Patrick McGoldrick <pmcgoldrick@gmail.com>
2019-01-03 14:40:16 -0800

    Initial Commit

---
+++ ML Image Classifier/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ML Image Classifier
+//
+//  Created by dirtbag on 1/3/19.
+//  Copyright © 2019 dirtbag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
