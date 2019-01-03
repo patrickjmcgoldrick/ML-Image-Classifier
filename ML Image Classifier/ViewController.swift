55c21c4ea7f49d00def3ab0d0f8e79c22ad08195
Patrick McGoldrick <pmcgoldrick@gmail.com>
2019-01-03 15:03:14 -0800

    feat: Resnet50 added as the Machine Learning model.  UI built around classification.

--- ML Image Classifier/ViewController.swift
+++ ML Image Classifier/ViewController.swift
@@ -7,14 +7,76 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var lblConfidence: UILabel!
+    @IBOutlet weak var lblClassification: UILabel!
+    
+    let imagePicker = UIImagePickerController()
+    
+    let resnetModel = Resnet50()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        
+        if let image = imageView.image {
+            processImage(image: image)
+        }
     }
-
-
+    
+    func processImage(image: UIImage) {
+        
+        if let model = try? VNCoreMLModel(for: self.resnetModel.model) {
+            let request = VNCoreMLRequest(model: model) { (request, error) in
+                if let results = request.results as? [VNClassificationObservation] {
+                    for classification in results {
+                        print("ID: \(classification.identifier) Confidence: \(classification.confidence)")
+                    }
+                    
+                    if let observation = results.first {
+                        self.lblClassification.text = observation.identifier
+                        let confidence = round(observation.confidence * 1000.0) / 10.0
+                        self.lblConfidence.text = "\(confidence)%"
+                    }
+                }
+                
+            }
+            
+            if let imageData = image.pngData() {
+                let handler = VNImageRequestHandler(data: imageData, options: [:])
+                try? handler.perform([request])
+                
+            }
+            
+        }
+        
+    }
+    
+    
+    @IBAction func actionTakePhoto(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func actionFindImages(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = selectedImage
+            processImage(image: selectedImage)
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
 
